20768fee998f5861060bccebe14834597656a59a
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-27 19:17:01 -0400

    Last condition

--- lab2/js/missionDecisionEngine.js
+++ lab2/js/missionDecisionEngine.js
@@ -13,24 +13,29 @@ if(health < 30 && enemyNearby === true ){
 
 //LOW RESOURCES WARNING
 //ammo < 5 OR shield is false
-if(ammo < 5 || shield === false){
+else if(ammo < 5 || shield === false){
     //"Warning: Low Resources"
 }
 
 //MID MISSION STATUS
 //missionProgress is between 1 and 70
-if(missionProgress > 1 || missionProgress < 70){
+else if(missionProgress > 1 || missionProgress < 70){
     //"Mission In Progress"
 }
 
 //HIGH PROGRESS STATUS
 //missionProgress > 70 AND enemyNearby is false
-if(missionProgress > 70 && enemyNearby === false){
+else if(missionProgress > 70 && enemyNearby === false){
     //"Approaching Mission Completion"
 }
 
 //MISSION COMPLETE
 //missionProgress === 100
-if(missionProgress === 100){
+else if(missionProgress === 100){
     //"Mission Complete Successfully"
+}
+
+//DEFAULT STATE
+else{
+    //"System Stable"
 }
