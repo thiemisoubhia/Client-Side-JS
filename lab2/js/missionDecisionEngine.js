92ff4b78659d33716e47f1f9aff2cd6bd22d3015
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-27 19:10:58 -0400

    Second condition

--- lab2/js/missionDecisionEngine.js
+++ lab2/js/missionDecisionEngine.js
@@ -6,8 +6,14 @@ let missionProgress = 40;
 let enemyNearby = true;
  
 //CRITICAL FAILURE (Highest Priority)
-
 //health < 30 AND enemyNearby is true
 if(health < 30 && enemyNearby === true ){
-
+    //"CRITICAL ALERT: Immediate Evacuation Required"
 }
+
+//LOW RESOURCES WARNING
+//ammo < 5 OR shield is false
+if(ammo < 5 || shield === false){
+    //"Warning: Low Resources"
+}
+
