345c8a009ed3c1b94b2e1bddab69e98bf3565b65
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-27 19:07:32 -0400

    First condition

--- lab2/missionDecisionEngine.js
+++ lab2/missionDecisionEngine.js
@@ -5,3 +5,9 @@ let shield = true;
 let missionProgress = 40;
 let enemyNearby = true;
  
+//CRITICAL FAILURE (Highest Priority)
+
+//health < 30 AND enemyNearby is true
+if(health < 30 && enemyNearby === true ){
+
+}
