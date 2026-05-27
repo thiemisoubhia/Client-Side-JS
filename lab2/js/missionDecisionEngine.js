cb1063e90c152c166e56d2a8c86a5982afa86684
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-27 19:23:38 -0400

    Change status

--- lab2/js/missionDecisionEngine.js
+++ lab2/js/missionDecisionEngine.js
@@ -13,33 +13,39 @@ let status = document.querySelector("#status");
 //health < 30 AND enemyNearby is true
 if(health < 30 && enemyNearby === true ){
     //"CRITICAL ALERT: Immediate Evacuation Required"
+    status.textContent = "CRITICAL ALERT: Immediate Evacuation Required";
 }
 
 //LOW RESOURCES WARNING
 //ammo < 5 OR shield is false
 else if(ammo < 5 || shield === false){
     //"Warning: Low Resources"
+    status.textContent = "Warning: Low Resources";
 }
 
 //MID MISSION STATUS
 //missionProgress is between 1 and 70
 else if(missionProgress > 1 && missionProgress < 70){
     //"Mission In Progress"
+    status.textContent = "Mission In Progress";
 }
 
 //HIGH PROGRESS STATUS
 //missionProgress > 70 AND enemyNearby is false
 else if(missionProgress > 70 && enemyNearby === false){
     //"Approaching Mission Completion"
+    status.textContent = "Approaching Mission Completion";
 }
 
 //MISSION COMPLETE
 //missionProgress === 100
 else if(missionProgress === 100){
     //"Mission Complete Successfully"
+    status.textContent = "Mission Complete Successfully";
 }
 
 //DEFAULT STATE
 else{
     //"System Stable"
+    status.textContent = "System Stable";
 }
