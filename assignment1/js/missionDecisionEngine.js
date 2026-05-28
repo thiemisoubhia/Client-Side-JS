f96d7646bb782802a657c0426b83bc398feac45c
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-28 13:49:02 -0400

    Reset funcitons and variables

--- assignment1/js/missionDecisionEngine.js
+++ assignment1/js/missionDecisionEngine.js
@@ -3,7 +3,7 @@
 
 //Controls
 const agents = ["Spy", "Sniper", "Cyber", "Controller", "Initiator"];
-const locations = ["City", "Base", "Arctic", "Enemy","Disney"];
+const locations = ["City", "Base", "Arctic", "Enemy", "Disney"];
 const weapons = ["Pistol", "Knife", "Rifle", "Drone"];
 const objectives = ["Extract Target", "Sabotage System", "Intel Retrieval", "Eliminate Threat"];
 const risks = ["Low", "Medium", "High", "Extreme"];
@@ -20,8 +20,24 @@ document.getElementById("launchBtn").addEventListener("click", generateMission);
 document.getElementById("resetBtn").addEventListener("click", resetMission);
 document.getElementById("randomBtn").addEventListener("click", randomMission);
 
+//variables
+let agent = null;
+let location = null;
+let weapon = null;
+let objective = null;
+let risk = null;
+
 //function to get the next value from the array
 function nextValue(array, current) {
     let index = array.indexOf(current);
     return array[(index + 1) % array.length];
+}
+
+//reset
+function resetMission() {
+    agent = null;
+    location = null;
+    weapon = null;
+    objective = null;
+    risk = null;
 }
