0cd6b93893b59725f854bfc1df9c08fc0aef168d
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-28 13:45:06 -0400

    nextValue function

--- assignment1/js/missionDecisionEngine.js
+++ assignment1/js/missionDecisionEngine.js
@@ -9,12 +9,19 @@ const objectives = ["Extract Target", "Sabotage System", "Intel Retrieval", "Eli
 const risks = ["Low", "Medium", "High", "Extreme"];
 
 // Get Elements HTML
-document.getElementById("agentBtn").addEventListener("click", cycleAgent);
-document.getElementById("locationBtn").addEventListener("click", cycleLocation);
-document.getElementById("weaponBtn").addEventListener("click", cycleWeapon);
-document.getElementById("objectiveBtn").addEventListener("click", cycleObjective);
-document.getElementById("riskBtn").addEventListener("click", cycleRisk);
+document.getElementById("agentBtn").addEventListener("click", funAgent);
+document.getElementById("locationBtn").addEventListener("click", funAgent);
+document.getElementById("weaponBtn").addEventListener("click", funWeapon);
+document.getElementById("objectiveBtn").addEventListener("click", funObject);
+document.getElementById("riskBtn").addEventListener("click", funRisk);
+
+//other buttons
 document.getElementById("launchBtn").addEventListener("click", generateMission);
 document.getElementById("resetBtn").addEventListener("click", resetMission);
 document.getElementById("randomBtn").addEventListener("click", randomMission);
 
+//function to get the next value from the array
+function nextValue(array, current) {
+    let index = array.indexOf(current);
+    return array[(index + 1) % array.length];
+}
