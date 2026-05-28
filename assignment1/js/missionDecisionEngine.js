c7eead910a782fce1b8334955284e10d50a07e1c
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-28 15:01:16 -0400

    Lauch funtion and corrections

--- assignment1/js/missionDecisionEngine.js
+++ assignment1/js/missionDecisionEngine.js
@@ -10,7 +10,7 @@ const risks = ["Low", "Medium", "High", "Extreme"];
 
 // Get Elements HTML
 document.getElementById("agentBtn").addEventListener("click", funAgent);
-document.getElementById("locationBtn").addEventListener("click", funAgent);
+document.getElementById("locationBtn").addEventListener("click", funLocation);
 document.getElementById("weaponBtn").addEventListener("click", funWeapon);
 document.getElementById("objectiveBtn").addEventListener("click", funObjective);
 document.getElementById("riskBtn").addEventListener("click", funRisk);
@@ -22,10 +22,13 @@ document.getElementById("randomBtn").addEventListener("click", randomMission);
 
 //variables
 let agent = null;
-let location = null;
+let loc= null;
 let weapon = null;
 let objective = null;
 let risk = null;
+
+// calling
+updateStatus();
 
 //function to get the next value from the array
 function nextValue(array, current) {
@@ -36,7 +39,7 @@ function nextValue(array, current) {
 //reset
 function resetMission() {
     agent = null;
-    location = null;
+    loc = null;
     weapon = null;
     objective = null;
     risk = null;
@@ -49,7 +52,7 @@ function resetMission() {
 function updateStatus() {
     document.getElementById("status").innerHTML =
     `Agent: ${agent} <br>
-     Location: ${location} <br>
+     Location: ${loc} <br>
      Weapon: ${weapon} <br>
      Objective: ${objective} <br>
      Risk: ${risk}`;
@@ -62,7 +65,7 @@ function funAgent() {
 }
 
 function funLocation() {
-    location = nextValue(locations, location);
+    loc = nextValue(locations, loc);
     updateStatus();
 }
 
@@ -79,4 +82,19 @@ function funObjective() {
 function funRisk() {
     risk = nextValue(risks, risk);
     updateStatus();
+}
+
+function generateMission() {
+    if (!agent || !loc || !weapon || !objective || !risk) {
+        document.getElementById("status").textContent =
+            "WARNING: Complete all mission parameters before launch!";
+        return;
+    }
+
+    document.getElementById("status").innerHTML =
+        `MISSION BRIEFING:<br>
+        Agent ${agent} assigned to ${loc}.<br>
+        Objective: ${objective}.<br>
+        Loadout: ${weapon}.<br>
+        Risk Level: ${risk}.`;
 }
