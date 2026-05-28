1ab81d114e0452373a3f1c1643d54d21fb462075
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-05-27 22:41:28 -0400

    Get Elements HTML by ID

--- assignment1/js/missionDecisionEngine.js
+++ assignment1/js/missionDecisionEngine.js
@@ -8,3 +8,13 @@ const weapons = ["Pistol", "Knife", "Rifle", "Drone"];
 const objectives = ["Extract Target", "Sabotage System", "Intel Retrieval", "Eliminate Threat"];
 const risks = ["Low", "Medium", "High", "Extreme"];
 
+// Get Elements HTML
+document.getElementById("agentBtn").addEventListener("click", cycleAgent);
+document.getElementById("locationBtn").addEventListener("click", cycleLocation);
+document.getElementById("weaponBtn").addEventListener("click", cycleWeapon);
+document.getElementById("objectiveBtn").addEventListener("click", cycleObjective);
+document.getElementById("riskBtn").addEventListener("click", cycleRisk);
+document.getElementById("launchBtn").addEventListener("click", generateMission);
+document.getElementById("resetBtn").addEventListener("click", resetMission);
+document.getElementById("randomBtn").addEventListener("click", randomMission);
+
