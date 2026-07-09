02245c25fa14a9994c95e025bb8479f7892ff307
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-07-09 06:02:46 -0400

    Student informations with js

---
+++ lab3/26S-JavaScript-02-Week09-main/assignment3/js/assignment.js
@@ -0,0 +1,6 @@
+//student infos
+const studentName = "Thiemi Soubhia Doi";
+const studentId = "200645138";
+
+document.getElementById("infos").textContent =
+    `Student Name: ${studentName} - Student ID: ${studentId}`;
