5d0468c1f0a3cb26cb8ad03067059a29d9773194
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-07-09 06:16:42 -0400

    Pizza class and description method

--- lab3/26S-JavaScript-02-Week09-main/assignment3/js/assignment.js
+++ lab3/26S-JavaScript-02-Week09-main/assignment3/js/assignment.js
@@ -2,5 +2,38 @@
 const studentName = "Thiemi Soubhia Doi";
 const studentId = "200645138";
 
-document.getElementById("infos").textContent =
-    `Student Name: ${studentName} - Student ID: ${studentId}`;
+document.getElementById("infos").textContent = `Student Name: ${studentName} - Student ID: ${studentId}`;
+
+//Pizza Class
+class Pizza {
+
+    constructor(customerName, email, phone, address, size, crust, toppings, instructions) {
+        this.customerName = customerName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+        this.instructions = instructions;
+    }
+
+
+    //pizza description
+    getDescription() {
+        let description =  `Customer Information:
+        Name: ${this.customerName}
+        Email: ${this.email}
+        Phone: ${this.phone}
+        Address: ${this.address}
+
+        Pizza Order:
+        Size: ${this.size}
+        Crust: ${this.crust}
+        Toppings: ${this.toppings.join(", ")}
+        Instructions: ${this.instructions || "None"}
+        `;
+
+        return description;
+    }
+}
