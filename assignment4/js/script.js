84ae824d2c5a7abf116c7c269480e3833819a507
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2026-07-19 22:07:48 -0400

    html elements & infos

--- assignment4/js/script.js
+++ assignment4/js/script.js
@@ -4,3 +4,15 @@ const key = "85223393794463e1b9ca9841d03246fc";
 
 //API url
 const url = "http://ws.audioscrobbler.com/2.0/";
+
+// My information
+const name  = "Thiemi Soubhia";
+const studentID = "200645138";
+
+// HTML elements
+const searchForm = document.querySelector("#search");
+const artistInput = document.querySelector("#artistName");
+const artist = document.querySelector("#artist");
+const myName = document.querySelector("#name");
+const myID = document.querySelector("#studentId");
+
